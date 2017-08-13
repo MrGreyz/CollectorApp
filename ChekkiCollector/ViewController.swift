d1b5d7a6c847b6d905099dd8d983ca298c1c250e
Armani Cesar <armanicesar123@live.com>
2017-08-13 14:23:34 +0100

    Initial Commit

---
+++ ChekkiCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChekkiCollector
+//
+//  Created by Armani on 13/08/2017.
+//  Copyright © 2017 Armani Cesar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
